2ceb1b37951632eae4caa2a075d4e99cc8e73e7e
mikoDom <mikoz7388@gmail.com>
2023-08-25 11:03:55 +0200

    refactor: :white_check_mark: switched to axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { Button } from "@/components/ui/button";
-import { getMovies } from "@/lib/api";
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
+import { apiClient } from "./lib/api";
+import { MovieList } from "./types";
 
 function App() {
-  const { data } = useQuery(["movies"], getMovies);
-
+  const { data } = useQuery(["movies"], async () => {
+    const response = await apiClient.get("/trending/movie/day");
+    return response.data as MovieList;
+  });
   console.log(data);
   return (
     <>
       <Header />
-      {data
+      {/* {data
         ? data.results.map((movie) => <h1 key={movie.id}>{movie.title}</h1>)
-        : null}
+        : null} */}
+      {/* {data ? JSON.stringify(data.results[0], null, 2) : null} */}
       <Button>elo</Button>
       <Footer />
     </>
--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,18 +1,18 @@
-import { MovieList } from "@/types";
+import axios from "axios";
 
-export const base = "https://api.movies.tastejs.com";
-export const media_base = "https://image.tmdb.org/t/p";
+export const base = "https://api.themoviedb.org/3";
+
+export const apiClient = axios.create({
+  baseURL: base,
+  headers: {
+    Accept: "application/json",
+    Authorization: import.meta.env.VITE_TMDB_API_KEY,
+  },
+});
 
 export function getURL(endpoint: string, params?: Record<string, string>) {
   const q = new URLSearchParams(params);
   const url = `${base}/${endpoint}?${q}`;
 
   return url;
-}
-
-export async function getMovies() {
-  const response = await fetch(getURL("trending/movie/day"));
-  const data = await response.json();
-
-  return data as MovieList;
 }
