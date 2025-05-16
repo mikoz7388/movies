e3e912f922590ff557babeb779e4e8ca95dd3783
miki <mikoz7388@gmail.com>
2025-05-16 18:52:14 +0200

    feat: :sparkles: implement scroll-based header opacity

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -2,12 +2,21 @@ import { Link, useNavigate } from "react-router-dom";
 import { ModeToggle } from "@/components/shared/mode-toggle";
 import { Input } from "@/components/ui/input";
 import logo from "@/assets/logo.svg";
-import { ChangeEvent, useState, FormEvent } from "react";
+import { ChangeEvent, useState, FormEvent, useEffect } from "react";
 import { X, Search } from "lucide-react";
 
 export function Header() {
   const [query, setQuery] = useState("");
+  const [atTop, setAtTop] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setAtTop(window.scrollY < 10);
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
     setQuery(e.target.value);
@@ -24,12 +33,21 @@ export function Header() {
   const handleClear = () => setQuery("");
 
   return (
-    <header className="sticky top-0 z-50 border border-b-foreground bg-background shadow-lg">
+    <header
+      className={`sticky top-0 z-50 flex items-center bg-background py-4 transition-all duration-200 ${
+        atTop
+          ? "border-b border-b-foreground opacity-100"
+          : "border-b-0 opacity-80"
+      }`}
+    >
+      <Link
+        to="/"
+        className={`ml-8 w-14 ${atTop ? "scale-100" : "scale-[0.8]"}`}
+      >
+        <img src={logo} alt="Cool Movies logo" />
+      </Link>
       <div className="container mx-auto flex w-full items-center justify-between p-4">
-        <div className="flex items-center gap-4">
-          <Link to="/">
-            <img src={logo} alt="Cool Movies logo" className="w-14" />
-          </Link>
+        <div className="flex items-center  gap-4">
           <form
             onSubmit={handleSearch}
             role="search"
--- src/components/ui/loading-spiner.tsx
+++ src/components/ui/loading-spiner.tsx
@@ -1,6 +1,6 @@
 export const LoadingSpinner = () => {
   return (
-    <div className="flex size-full items-center justify-center">
+    <div className="size-full flex items-center justify-center">
       <span className="sr-only">loading</span>
       <div className="size-16 animate-spin rounded-full border-t-4 border-blue-500 border-opacity-75"></div>
     </div>
